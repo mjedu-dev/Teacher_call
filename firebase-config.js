2cfdd937e1e7218846fdf0d272b05b5ff4dea2d1
minjeong <happyeducom1999@gmail.com>
2026-06-09 18:15:56 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@
 // 실제 Firebase 프로젝트를 생성한 후 아래 객체의 값을 채워주세요.
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBdylK3CixCC9pKN1GfK7vXF4SJi3AwjU8",
+  authDomain: "school-paging-248e2.firebaseapp.com",
+  projectId: "school-paging-248e2",
+  storageBucket: "school-paging-248e2.firebasestorage.app",
+  messagingSenderId: "797787101106",
+  appId: "1:797787101106:web:d7d067cd8cbf688a9df6ab"
 };
 
 // Firebase 설정이 유효한지 확인하는 함수
